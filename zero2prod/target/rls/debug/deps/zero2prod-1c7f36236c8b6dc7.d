c4a22a2c2a53c7450f3f26bfb40f061bb84a5da7
Joshua <3e2.weechongjingjoshua43@gmail.com>
2022-09-03 22:52:21 +0800

    Project skeleton

---
+++ zero2prod/target/rls/debug/deps/zero2prod-1c7f36236c8b6dc7.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/65989/zero2prod/target/rls/debug/deps/zero2prod-1c7f36236c8b6dc7.rmeta: src/main.rs
+
+/mnt/c/Users/65989/zero2prod/target/rls/debug/deps/zero2prod-1c7f36236c8b6dc7.d: src/main.rs
+
+src/main.rs:
---
+++ zero2prod/target/rls/debug/deps/zero2prod-db53c6f7757aa2d8.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/65989/zero2prod/target/rls/debug/deps/zero2prod-db53c6f7757aa2d8.rmeta: src/main.rs
+
+/mnt/c/Users/65989/zero2prod/target/rls/debug/deps/zero2prod-db53c6f7757aa2d8.d: src/main.rs
+
+src/main.rs:
